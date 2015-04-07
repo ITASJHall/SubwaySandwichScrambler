843f6376bcdb0059e835f4cbc9778e2c56e259a1
Johnathan <Johnathan.hall@itas.ca>
2015-04-07 15:07:50 -0700

    Unstaching Facebook SDK

--- app/src/main/java/com/logobuico/johnathan/subwaysandwichscrambler/MainActivity.java
+++ app/src/main/java/com/logobuico/johnathan/subwaysandwichscrambler/MainActivity.java
@@ -217,7 +217,7 @@ public class MainActivity extends ListActivity  {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
